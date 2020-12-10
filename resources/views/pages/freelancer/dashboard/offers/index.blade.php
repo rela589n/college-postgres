0752efc8e8d3a90de268e2adea419ab731091308
rela589n <zsidelnik@gmail.com>
2020-12-10 20:12:12 +0200

    order columns

--- resources/views/pages/freelancer/dashboard/offers/index.blade.php
+++ resources/views/pages/freelancer/dashboard/offers/index.blade.php
@@ -15,9 +15,21 @@
         <x-texts.h1>{{ trans('texts.dashboard.offers.index.title') }}</x-texts.h1>
         <x-entities.offers.table-for-freelancers :jobs="$offers"/>
         <form method="GET">
-            <input class="mr-5" type="text" name="filters[title]" value="{{ request('filters.title') }}">
-            <input class="mx-5" type="text" name="filters[description]" value="{{ request('filters.description') }}">
-            <input type="submit" value="Filter">
+            <div class="my-4 d-flex justify-content-between">
+                <div class="d-flex flex-column">
+                    <input type="text" name="filters[title]" value="{{ request('filters.title') }}">
+                    <label class="mt-2">DESC <input type="checkbox" name="desc[title]"
+                                       @if(request('desc.title')) checked="checked" @endif></label>
+                </div>
+                <div class="d-flex flex-column">
+                    <input type="text" name="filters[description]" value="{{ request('filters.description') }}">
+                    <label class="mt-2">DESC <input type="checkbox" name="desc[description]"
+                                       @if(request('desc.description')) checked="checked" @endif></label>
+                </div>
+                <div>
+                    <input type="submit" value="Filter">
+                </div>
+            </div>
         </form>
     </x-grid.dashboard.main>
 @endsection
