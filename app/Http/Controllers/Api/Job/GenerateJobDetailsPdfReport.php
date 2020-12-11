f91806658ac1343a864809fa54149ca7b54571f8
rela589n <zsidelnik@gmail.com>
2020-12-11 13:52:30 +0200

    set up GenerateJobDetailsPdfReport

---
+++ app/Http/Controllers/Api/Job/GenerateJobDetailsPdfReport.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\Job;
+
+
+use App\Http\Controllers\Controller;
+
+class GenerateJobDetailsPdfReport extends Controller
+{
+    public function __invoke()
+    {
+        return 1;
+    }
+}
